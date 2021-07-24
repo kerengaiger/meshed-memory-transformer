ebf6d53902f8b76fb2da9513887ea35dd0591742
kerengaiger <keren.gaiger@gmail.com>
2021-07-24 16:40:26 +0300

    train: change images to coco/images

--- train.py
+++ train.py
@@ -165,159 +165,159 @@ if __name__ == '__main__':
                            remove_punctuation=True, nopoints=False)
 
     # Create the dataset
-    dataset = COCO(image_field, text_field, 'images/', args.annotation_folder, args.annotation_folder)
+    dataset = COCO(image_field, text_field, 'coco/images/', args.annotation_folder, args.annotation_folder)
     train_dataset, val_dataset, test_dataset = dataset.splits
 
-    # if not os.path.isfile('vocab_%s.pkl' % args.exp_name):
-    #     print("Building vocabulary")
-    #     text_field.build_vocab(train_dataset, val_dataset, min_freq=5)
-    #     pickle.dump(text_field.vocab, open('vocab_%s.pkl' % args.exp_name, 'wb'))
-    # else:
-    #     text_field.vocab = pickle.load(open('vocab_%s.pkl' % args.exp_name, 'rb'))
-    #
-    # # Model and dataloaders
-    # encoder = MemoryAugmentedEncoder(3, 0, attention_module=ScaledDotProductAttentionMemory,
-    #                                  attention_module_kwargs={'m': args.m})
-    # decoder = MeshedDecoder(len(text_field.vocab), 54, 3, text_field.vocab.stoi['<pad>'])
-    # model = Transformer(text_field.vocab.stoi['<bos>'], encoder, decoder).to(device)
-    #
-    # dict_dataset_train = train_dataset.image_dictionary({'image': image_field, 'text': RawField()})
-    # ref_caps_train = list(train_dataset.text)
-    # cider_train = Cider(PTBTokenizer.tokenize(ref_caps_train))
-    # dict_dataset_val = val_dataset.image_dictionary({'image': image_field, 'text': RawField()})
-    # dict_dataset_test = test_dataset.image_dictionary({'image': image_field, 'text': RawField()})
-    #
-    #
-    # def lambda_lr(s):
-    #     warm_up = args.warmup
-    #     s += 1
-    #     return (model.d_model ** -.5) * min(s ** -.5, s * warm_up ** -1.5)
-    #
-    #
-    # # Initial conditions
-    # optim = Adam(model.parameters(), lr=1, betas=(0.9, 0.98))
-    # scheduler = LambdaLR(optim, lambda_lr)
-    # loss_fn = NLLLoss(ignore_index=text_field.vocab.stoi['<pad>'])
-    # use_rl = False
-    # best_cider = .0
-    # patience = 0
-    # start_epoch = 0
-    #
-    # if args.resume_last or args.resume_best:
-    #     if args.resume_last:
-    #         fname = 'saved_models/%s_last.pth' % args.exp_name
-    #     else:
-    #         fname = 'saved_models/%s_best.pth' % args.exp_name
-    #
-    #     if os.path.exists(fname):
-    #         data = torch.load(fname)
-    #         torch.set_rng_state(data['torch_rng_state'])
-    #         torch.cuda.set_rng_state(data['cuda_rng_state'])
-    #         np.random.set_state(data['numpy_rng_state'])
-    #         random.setstate(data['random_rng_state'])
-    #         model.load_state_dict(data['state_dict'], strict=False)
-    #         optim.load_state_dict(data['optimizer'])
-    #         scheduler.load_state_dict(data['scheduler'])
-    #         start_epoch = data['epoch'] + 1
-    #         best_cider = data['best_cider']
-    #         patience = data['patience']
-    #         use_rl = data['use_rl']
-    #         print('Resuming from epoch %d, validation loss %f, and best cider %f' % (
-    #             data['epoch'], data['val_loss'], data['best_cider']))
-    #
-    # print("Training starts")
-    # for e in range(start_epoch, start_epoch + 100):
-    #     dataloader_train = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers,
-    #                                   drop_last=True)
-    #     dataloader_val = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
-    #     dict_dataloader_train = DataLoader(dict_dataset_train, batch_size=args.batch_size // 5, shuffle=True,
-    #                                        num_workers=args.workers)
-    #     dict_dataloader_val = DataLoader(dict_dataset_val, batch_size=args.batch_size // 5)
-    #     dict_dataloader_test = DataLoader(dict_dataset_test, batch_size=args.batch_size // 5)
-    #
-    #     if not use_rl:
-    #         train_loss = train_xe(model, dataloader_train, optim, text_field)
-    #         writer.add_scalar('data/train_loss', train_loss, e)
-    #     else:
-    #         train_loss, reward, reward_baseline = train_scst(model, dict_dataloader_train, optim, cider_train, text_field)
-    #         writer.add_scalar('data/train_loss', train_loss, e)
-    #         writer.add_scalar('data/reward', reward, e)
-    #         writer.add_scalar('data/reward_baseline', reward_baseline, e)
-    #
-    #     # Validation loss
-    #     val_loss = evaluate_loss(model, dataloader_val, loss_fn, text_field)
-    #     writer.add_scalar('data/val_loss', val_loss, e)
-    #
-    #     # Validation scores
-    #     scores = evaluate_metrics(model, dict_dataloader_val, text_field)
-    #     print("Validation scores", scores)
-    #     val_cider = scores['CIDEr']
-    #     writer.add_scalar('data/val_cider', val_cider, e)
-    #     writer.add_scalar('data/val_bleu1', scores['BLEU'][0], e)
-    #     writer.add_scalar('data/val_bleu4', scores['BLEU'][3], e)
-    #     writer.add_scalar('data/val_meteor', scores['METEOR'], e)
-    #     writer.add_scalar('data/val_rouge', scores['ROUGE'], e)
-    #
-    #     # Test scores
-    #     scores = evaluate_metrics(model, dict_dataloader_test, text_field)
-    #     print("Test scores", scores)
-    #     writer.add_scalar('data/test_cider', scores['CIDEr'], e)
-    #     writer.add_scalar('data/test_bleu1', scores['BLEU'][0], e)
-    #     writer.add_scalar('data/test_bleu4', scores['BLEU'][3], e)
-    #     writer.add_scalar('data/test_meteor', scores['METEOR'], e)
-    #     writer.add_scalar('data/test_rouge', scores['ROUGE'], e)
-    #
-    #     # Prepare for next epoch
-    #     best = False
-    #     if val_cider >= best_cider:
-    #         best_cider = val_cider
-    #         patience = 0
-    #         best = True
-    #     else:
-    #         patience += 1
-    #
-    #     switch_to_rl = False
-    #     exit_train = False
-    #     if patience == 5:
-    #         if not use_rl:
-    #             use_rl = True
-    #             switch_to_rl = True
-    #             patience = 0
-    #             optim = Adam(model.parameters(), lr=5e-6)
-    #             print("Switching to RL")
-    #         else:
-    #             print('patience reached.')
-    #             exit_train = True
-    #
-    #     if switch_to_rl and not best:
-    #         data = torch.load('saved_models/%s_best.pth' % args.exp_name)
-    #         torch.set_rng_state(data['torch_rng_state'])
-    #         torch.cuda.set_rng_state(data['cuda_rng_state'])
-    #         np.random.set_state(data['numpy_rng_state'])
-    #         random.setstate(data['random_rng_state'])
-    #         model.load_state_dict(data['state_dict'])
-    #         print('Resuming from epoch %d, validation loss %f, and best cider %f' % (
-    #             data['epoch'], data['val_loss'], data['best_cider']))
-    #
-    #     torch.save({
-    #         'torch_rng_state': torch.get_rng_state(),
-    #         'cuda_rng_state': torch.cuda.get_rng_state(),
-    #         'numpy_rng_state': np.random.get_state(),
-    #         'random_rng_state': random.getstate(),
-    #         'epoch': e,
-    #         'val_loss': val_loss,
-    #         'val_cider': val_cider,
-    #         'state_dict': model.state_dict(),
-    #         'optimizer': optim.state_dict(),
-    #         'scheduler': scheduler.state_dict(),
-    #         'patience': patience,
-    #         'best_cider': best_cider,
-    #         'use_rl': use_rl,
-    #     }, 'saved_models/%s_last.pth' % args.exp_name)
-    #
-    #     if best:
-    #         copyfile('saved_models/%s_last.pth' % args.exp_name, 'saved_models/%s_best.pth' % args.exp_name)
-    #
-    #     if exit_train:
-    #         writer.close()
-    #         break
+    if not os.path.isfile('vocab_%s.pkl' % args.exp_name):
+        print("Building vocabulary")
+        text_field.build_vocab(train_dataset, val_dataset, min_freq=5)
+        pickle.dump(text_field.vocab, open('vocab_%s.pkl' % args.exp_name, 'wb'))
+    else:
+        text_field.vocab = pickle.load(open('vocab_%s.pkl' % args.exp_name, 'rb'))
+
+    # Model and dataloaders
+    encoder = MemoryAugmentedEncoder(3, 0, attention_module=ScaledDotProductAttentionMemory,
+                                     attention_module_kwargs={'m': args.m})
+    decoder = MeshedDecoder(len(text_field.vocab), 54, 3, text_field.vocab.stoi['<pad>'])
+    model = Transformer(text_field.vocab.stoi['<bos>'], encoder, decoder).to(device)
+
+    dict_dataset_train = train_dataset.image_dictionary({'image': image_field, 'text': RawField()})
+    ref_caps_train = list(train_dataset.text)
+    cider_train = Cider(PTBTokenizer.tokenize(ref_caps_train))
+    dict_dataset_val = val_dataset.image_dictionary({'image': image_field, 'text': RawField()})
+    dict_dataset_test = test_dataset.image_dictionary({'image': image_field, 'text': RawField()})
+
+
+    def lambda_lr(s):
+        warm_up = args.warmup
+        s += 1
+        return (model.d_model ** -.5) * min(s ** -.5, s * warm_up ** -1.5)
+
+
+    # Initial conditions
+    optim = Adam(model.parameters(), lr=1, betas=(0.9, 0.98))
+    scheduler = LambdaLR(optim, lambda_lr)
+    loss_fn = NLLLoss(ignore_index=text_field.vocab.stoi['<pad>'])
+    use_rl = False
+    best_cider = .0
+    patience = 0
+    start_epoch = 0
+
+    if args.resume_last or args.resume_best:
+        if args.resume_last:
+            fname = 'saved_models/%s_last.pth' % args.exp_name
+        else:
+            fname = 'saved_models/%s_best.pth' % args.exp_name
+
+        if os.path.exists(fname):
+            data = torch.load(fname)
+            torch.set_rng_state(data['torch_rng_state'])
+            torch.cuda.set_rng_state(data['cuda_rng_state'])
+            np.random.set_state(data['numpy_rng_state'])
+            random.setstate(data['random_rng_state'])
+            model.load_state_dict(data['state_dict'], strict=False)
+            optim.load_state_dict(data['optimizer'])
+            scheduler.load_state_dict(data['scheduler'])
+            start_epoch = data['epoch'] + 1
+            best_cider = data['best_cider']
+            patience = data['patience']
+            use_rl = data['use_rl']
+            print('Resuming from epoch %d, validation loss %f, and best cider %f' % (
+                data['epoch'], data['val_loss'], data['best_cider']))
+
+    print("Training starts")
+    for e in range(start_epoch, start_epoch + 100):
+        dataloader_train = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers,
+                                      drop_last=True)
+        dataloader_val = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+        dict_dataloader_train = DataLoader(dict_dataset_train, batch_size=args.batch_size // 5, shuffle=True,
+                                           num_workers=args.workers)
+        dict_dataloader_val = DataLoader(dict_dataset_val, batch_size=args.batch_size // 5)
+        dict_dataloader_test = DataLoader(dict_dataset_test, batch_size=args.batch_size // 5)
+
+        if not use_rl:
+            train_loss = train_xe(model, dataloader_train, optim, text_field)
+            writer.add_scalar('data/train_loss', train_loss, e)
+        else:
+            train_loss, reward, reward_baseline = train_scst(model, dict_dataloader_train, optim, cider_train, text_field)
+            writer.add_scalar('data/train_loss', train_loss, e)
+            writer.add_scalar('data/reward', reward, e)
+            writer.add_scalar('data/reward_baseline', reward_baseline, e)
+
+        # Validation loss
+        val_loss = evaluate_loss(model, dataloader_val, loss_fn, text_field)
+        writer.add_scalar('data/val_loss', val_loss, e)
+
+        # Validation scores
+        scores = evaluate_metrics(model, dict_dataloader_val, text_field)
+        print("Validation scores", scores)
+        val_cider = scores['CIDEr']
+        writer.add_scalar('data/val_cider', val_cider, e)
+        writer.add_scalar('data/val_bleu1', scores['BLEU'][0], e)
+        writer.add_scalar('data/val_bleu4', scores['BLEU'][3], e)
+        writer.add_scalar('data/val_meteor', scores['METEOR'], e)
+        writer.add_scalar('data/val_rouge', scores['ROUGE'], e)
+
+        # Test scores
+        scores = evaluate_metrics(model, dict_dataloader_test, text_field)
+        print("Test scores", scores)
+        writer.add_scalar('data/test_cider', scores['CIDEr'], e)
+        writer.add_scalar('data/test_bleu1', scores['BLEU'][0], e)
+        writer.add_scalar('data/test_bleu4', scores['BLEU'][3], e)
+        writer.add_scalar('data/test_meteor', scores['METEOR'], e)
+        writer.add_scalar('data/test_rouge', scores['ROUGE'], e)
+
+        # Prepare for next epoch
+        best = False
+        if val_cider >= best_cider:
+            best_cider = val_cider
+            patience = 0
+            best = True
+        else:
+            patience += 1
+
+        switch_to_rl = False
+        exit_train = False
+        if patience == 5:
+            if not use_rl:
+                use_rl = True
+                switch_to_rl = True
+                patience = 0
+                optim = Adam(model.parameters(), lr=5e-6)
+                print("Switching to RL")
+            else:
+                print('patience reached.')
+                exit_train = True
+
+        if switch_to_rl and not best:
+            data = torch.load('saved_models/%s_best.pth' % args.exp_name)
+            torch.set_rng_state(data['torch_rng_state'])
+            torch.cuda.set_rng_state(data['cuda_rng_state'])
+            np.random.set_state(data['numpy_rng_state'])
+            random.setstate(data['random_rng_state'])
+            model.load_state_dict(data['state_dict'])
+            print('Resuming from epoch %d, validation loss %f, and best cider %f' % (
+                data['epoch'], data['val_loss'], data['best_cider']))
+
+        torch.save({
+            'torch_rng_state': torch.get_rng_state(),
+            'cuda_rng_state': torch.cuda.get_rng_state(),
+            'numpy_rng_state': np.random.get_state(),
+            'random_rng_state': random.getstate(),
+            'epoch': e,
+            'val_loss': val_loss,
+            'val_cider': val_cider,
+            'state_dict': model.state_dict(),
+            'optimizer': optim.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'patience': patience,
+            'best_cider': best_cider,
+            'use_rl': use_rl,
+        }, 'saved_models/%s_last.pth' % args.exp_name)
+
+        if best:
+            copyfile('saved_models/%s_last.pth' % args.exp_name, 'saved_models/%s_best.pth' % args.exp_name)
+
+        if exit_train:
+            writer.close()
+            break
